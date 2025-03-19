0f01d0d6cbc6fa928fc1d1f32b3d38efcfeeb6ee
gou <19513317149@163.com>
2025-03-19 17:09:44 +0800

    【CMake的学习】提交动态库

--- build/lesson2_2/mul/cmake_install.cmake
+++ build/lesson2_2/mul/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/19513/Desktop/text
+# Install script for directory: C:/Users/19513/Desktop/text/lesson2_2/mul
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -37,34 +37,9 @@ if(NOT DEFINED CMAKE_OBJDUMP)
   set(CMAKE_OBJDUMP "D:/SDK/w64devkit/bin/objdump.exe")
 endif()
 
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for each subdirectory.
-  include("C:/Users/19513/Desktop/text/build/lesson1_1/cmake_install.cmake")
-  include("C:/Users/19513/Desktop/text/build/lesson1_2/cmake_install.cmake")
-  include("C:/Users/19513/Desktop/text/build/lesson2_1/cmake_install.cmake")
-  include("C:/Users/19513/Desktop/text/build/lesson2_2/cmake_install.cmake")
-
-endif()
-
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/19513/Desktop/text/build/install_local_manifest.txt"
-     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
-endif()
-if(CMAKE_INSTALL_COMPONENT)
-  if(CMAKE_INSTALL_COMPONENT MATCHES "^[a-zA-Z0-9_.+-]+$")
-    set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
-  else()
-    string(MD5 CMAKE_INST_COMP_HASH "${CMAKE_INSTALL_COMPONENT}")
-    set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INST_COMP_HASH}.txt")
-    unset(CMAKE_INST_COMP_HASH)
-  endif()
-else()
-  set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
-endif()
-
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/19513/Desktop/text/build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/19513/Desktop/text/build/lesson2_2/mul/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
---
+++ lesson2_2/main.cpp
@@ -0,0 +1,10 @@
+#include "iostream"
+#include "mul.hh"
+
+using namespace std;
+
+int main()
+{
+    cout << "lesson2_2" << endl;
+    // mul(1, 1);
+}
