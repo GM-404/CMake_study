f9f4a78974414adf0144a3d0cb14ae66cef6371d
gou <19513317149@163.com>
2025-04-01 11:41:02 +0800

    【Pybind11的学习】提交静态分析找不到自己封装的C++库文件

--- Test/test_add.py
+++ Test/test_add.py
@@ -2,9 +2,17 @@ import sys
 import os
 
 # 添加生成的模块路径
-sys.path.append(os.path.abspath("C:\\Users\\19513\\Desktop\\cmake\\build\\PythonWrapper"))
-import lesson
+sys.path.append(os.path.abspath(r"C:\\Users\\19513\\Desktop\\cmake\build\\PythonWrapper"))
+print(sys.path)  # 打印 sys.path 内容，检查路径是否添加正确
+
+try:
+	import lesson
+except ModuleNotFoundError:
+	print("The 'lesson' module could not be found. Ensure it is built and located in the specified path.")
+	sys.exit(1)
 
 a = 0
 a = lesson.add(3, 2)
 print(a)
+
+
