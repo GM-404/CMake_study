ae447c4a026b8264183a48bab8a94bfd4b06910d
gou <19513317149@163.com>
2026-04-22 10:20:39 +0800

    提交35. 二分查找相关题目

---
+++ lesson3_1/include/binary_search.hh
@@ -0,0 +1,13 @@
+#ifndef _BINARY_SEARCH_HH_
+#define _BINARY_SEARCH_HH_
+
+
+#include <vector>
+
+class BinarySearch{ 
+public:
+    //35. 搜索插入位置
+    int searchInsert(std::vector<int>& nums, int target);
+
+};
+#endif
