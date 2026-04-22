7aa3fb7d5bbd02a67e683cc9dfc9474c7b3e4b3e
gou <19513317149@163.com>
2026-04-22 16:46:58 +0800

    提交394

--- lesson3_1/include/stack.hh
+++ lesson3_1/include/stack.hh
@@ -7,6 +7,41 @@ class stack{
 public:
     //20.有效的括号
     bool isValid(string s);
+    //394.字符串解码
+    std::string decodeString(string s);
+
+};
+
+//155.最小栈
+class MinStack{
+private:
+    std::stack<int> st;     //正常存数据
+    std::stack<int> minSt;  //专门存最小值
+public:
+    MinStack(){
+    }
+
+    void push(int val){
+        st.push(val);
+        //最小栈为空，或者当前值更小，就压入
+        if(minSt.empty()||val<=minSt.top()){
+            minSt.push(val);
+        }
+    }
+    void pop(){
+        //如果栈顶就是最小值，最小栈也要弹出来
+        if(st.top()==minSt.top()){
+            minSt.pop();
+        }
+        st.pop();
+
+    }
+    int top(){
+        return st.top();
+    }
+    int getMin(){
+        return minSt.top();
+    }
 
 };
 #endif
--- lesson3_1/source/stack.cpp
+++ lesson3_1/source/stack.cpp
@@ -27,3 +27,44 @@ bool stack::isValid(string s){
     }
     return res.empty();
 }
+//394.字符串解码
+string stack:: decodeString(string s){
+    std::stack<int> nums_stack;        //存数字(倍数)
+    std::stack<std::string> al_stack;  //存之前的字符串
+    string res = "";                   //当前正在拼接的字符串
+    int num = 0;                       //当前的数字
+
+    for(char al:s){
+        //1.如果是数字(可能是多位)
+        if(isdigit(al))    
+        num = num*10 +(al - '0');
+        //遇到'['将当前数字与字母压进栈中，然后重置
+        else if(al=='['){
+            nums_stack.push(num);
+            al_stack.push(res);
+            //重置
+            num = 0;
+            res = "";
+        }
+        //遇到']'开始解码
+        else if(al == ']'){
+            int k = nums_stack.top();
+            nums_stack.pop();
+
+            string prestr = al_stack.top();  //这里是之前的a
+            al_stack.pop();
+
+            //把当前字符串重复K次，拼接到之前的字符串
+            string temp = "";
+            for(int i = 0;i<k;i++){
+                temp += res;
+            }
+            res = prestr + temp;
+        }
+        //普通字母
+        else{
+            res += al;
+        }
+    }
+    return res;
+}
